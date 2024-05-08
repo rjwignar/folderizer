74c59423d43f9a4eda18fb679e0abec5167bc629
Roy J. Wignarajah <78163326+rjwignar@users.noreply.github.com>
2024-05-08 19:01:48 -0400

    Add argparse command-line argument parser (#2)
    * added argparse command-line argument parser
    
    * added help, version arguments

--- folderizer/__init__.py
+++ folderizer/__init__.py
@@ -1,2 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="folderizer",
+    description="File organization command-line tool.",
+    epilog="Thanks for using %(prog)s!")
+parser.add_argument('filepath')
+parser.add_argument("-v","--version", action="version", version="%(prog)s 0.1.0")
+args = parser.parse_args()
+
 def example_function():
     return 1 + 1
