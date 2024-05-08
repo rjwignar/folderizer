25257d8cc9333e1d5a7c387879b3af044bcb6ae2
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-05-08 18:29:16 -0400

    added argparse

--- folderizer/__init__.py
+++ folderizer/__init__.py
@@ -1,2 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="folderizer",
+    description="File organization command-line tool.",
+    epilog="Thanks for using %(prog)s!")
+parser.add_argument('filepath')
+args = parser.parse_args()
+
 def example_function():
     return 1 + 1
